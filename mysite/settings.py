31edc3c3be0980d00ef29ca95cf1b499d42bc58b
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-01 13:58:46 +1200

    Heroku Fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv("SECRET_KEY")
+SECRET_KEY = '-pklp2vyme_mj99dvt#gzc!j+v5_$$7=uj!s&j@ug^_qix0wb'
+#os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
