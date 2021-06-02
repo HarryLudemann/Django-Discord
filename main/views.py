8556b781797833334dc7d419d10193246ff4ee9f
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-02 19:23:59 +1200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -50,7 +50,8 @@ def discordloginredirect(response):
     user = exchange_code(code)
     discord_user = authenticate(response, user=user)
     #discord_user = list(discord_user).pop()
-    login(response, list(discord_user).pop())
+    print(discord_user)
+    login(response,discord_user)
     return redirect("/")
 
 @login_required(login_url='/oauth2/login')
