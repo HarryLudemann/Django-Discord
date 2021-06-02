88ecbd80b99188351f7f2e3ee178a9699208a15a
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-02 22:07:09 +1200

    Fix

--- main/views.py
+++ main/views.py
@@ -39,10 +39,6 @@ def exchange_code(code):
 
 
 def home(response):
-    code = response.GET.get('code')
-    print(code)
-    user = exchange_code(code)
-    user = authenticate(response, user=user)
     return render(response, "main/home.html", {'user':user})
 
 def discordlogin(response):
