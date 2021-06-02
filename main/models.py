f86bc326e0f22f6259cf66f104a73cdcafa9741c
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-02 19:11:39 +1200

    Model Fix

--- main/models.py
+++ main/models.py
@@ -15,16 +15,3 @@ class Privileges(models.Model):
     adminchangeprefix = models.CharField(max_length=50)
     admintest = models.CharField(max_length=50)
 
-class DiscordUser(models.Model):
-    objects = DiscordUserOAuth2Manager()
-    id = models.BigIntegerField(primary_key=True)
-    discord_tag = models.CharField(max_length=100)
-    avatar = models.CharField(max_length=100)
-    public_flags = models.IntegerField()
-    flags = models.IntegerField()
-    locale = models.CharField(max_length=100)
-    mfa_enabled = models.BooleanField()
-    last_login = models.DateTimeField(null=True)
-
-    def is_authenticated(self, request):
-        return True
