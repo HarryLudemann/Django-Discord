3d5b42fa4ef37f754ac71b8ce441c1a4e50c8042
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-04 17:59:23 +1200

    Update context_processors.py

--- main/context_processors.py
+++ main/context_processors.py
@@ -17,6 +17,8 @@ def CheckDarkTheme(response):
 # Returns object of owned guilds
 def GetGuilds(response):
     obj = Guilds.objects.all()
+    for item in obj:
+        print(item.name)
     obj = obj.filter(userid=response.user.id)
     return obj
 
