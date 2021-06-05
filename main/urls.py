2729bc9f849652df59d422eab7c5e55fd104126d
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-05 21:28:04 +1200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('oauth2/login', views.discordlogin, name='discordlogin'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('oauth2/login/redirect', views.discordloginredirect, name='discordloginredirect'),
-    path('changeprivileges/<int:guildid>', views.changeprivileges, name='changeprivileges'),
+    path('changeprivileges/<int:id>', views.changeprivileges, name='changeprivileges'),
 ]
