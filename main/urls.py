85b6b95de57472e29d9115e234b28e74d41472dc
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-06-05 21:04:00 +1200

    Added url

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('oauth2/login', views.discordlogin, name='discordlogin'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('oauth2/login/redirect', views.discordloginredirect, name='discordloginredirect'),
     path('changeprivileges/<int:guildid>', views.changeprivileges, name='changeprivileges'),
 ]
